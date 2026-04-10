dbcaaba1232cfd5f92b284e45b7f40e253965fb0
virtyzz <romanpalblch93@gmail.com>
2026-04-11 04:28:03 +0700

    loop 1 minute and window 5

--- main.py
+++ main.py
@@ -74,12 +74,12 @@ async def on_ready():
     archive_daily.start()
 
 
-@tasks.loop(minutes=5)
+@tasks.loop(minutes=1)
 async def archive_daily():
     """Ежедневное архивирование данных в 00:00 MSK (21:00 UTC)"""
     now_utc = datetime.utcnow()
     print(f"[Archive check] UTC time: {now_utc.strftime('%H:%M:%S')}")
-    if now_utc.hour == 21 and now_utc.minute < 10:
+    if now_utc.hour == 21 and now_utc.minute < 5:
         if not hasattr(archive_daily, "last_run") or archive_daily.last_run != now_utc.date():
             print("Выполняется ежедневное архивирование данных...")
             archive_reports()
